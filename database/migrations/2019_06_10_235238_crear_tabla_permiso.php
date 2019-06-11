0c974487dd8cdbb2f0911d5e7d63875ca9f8e29d
Ing. Jarry Palacios <Ing. jarry.palacios@hotmail.com>
2019-06-10 21:47:20 -0400

    se creó migración del modelo ER

--- database/migrations/2019_06_10_235238_crear_tabla_permiso.php
+++ database/migrations/2019_06_10_235238_crear_tabla_permiso.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CrearTablaPermiso extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('permiso', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nombre',50)->unique();
+            $table->string('slug',50);
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permiso');
     }
 }
